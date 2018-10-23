ad8fd373ebc51eddd926e7522f4c42988f70fb43
Ognyan Chikov <o.chikov@propy.com>
2018-10-23 22:06:41 +0300

    classwork of connection to rinkeby and show our slogan in the interface that we created

--- Lecture_5/src/app/app.component.ts
+++ Lecture_5/src/app/app.component.ts
@@ -1,5 +1,7 @@
 declare let require: any;
 import { Component } from '@angular/core';
+import * as ethers from 'ethers';
+const Billboard = require('./contract_interfaces/Billboard.json');
 
 @Component({
   selector: 'dapp-root',
@@ -14,27 +16,39 @@ export class AppComponent {
   public privateKey: string;
   public currentBlock: number;
   public gasPrice: string;
+  public infuraApiKey = 'abca6d1110b443b08ef271545f24b80d';
+  public infuraProvider: ethers.providers.InfuraProvider;
+  public contractAddress = '0x78ed7A34D67fB3eAc745e7af78aE1bcA770C26de';
+  public deployedContract: ethers.Contract;
 
   constructor() {
-
+    this.infuraProvider = new ethers.providers.InfuraProvider('rinkeby', this.infuraApiKey);
+    this.infuraProvider.on('block', blockNumber => {
+      this.currentBlock = blockNumber;
+    });
+    this.deployedContract = new ethers.Contract(this.contractAddress, Billboard.abi, this.infuraProvider);
   }
 
   public async getCurrentBlock() {
-
+    this.currentBlock = await this.infuraProvider.getBlockNumber();
   }
 
   public async getGasPrice() {
-
+    const price = await this.infuraProvider.getGasPrice();
+    this.gasPrice = ethers.utils.formatEther(price.toNumber());
   }
 
   public async getBillboardContent() {
+    this.billboardContent = await this.deployedContract.slogan();
   }
 
   public async getTransactionHash(transactionHash) {
-
+    const transaction = await this.infuraProvider.getTransactionReceipt(transactionHash);
+    console.log(transaction);
   }
 
   public async moneySpent() {
-
+    const moneySpent = await this.deployedContract.moneySpent(this.address);
+    console.log(moneySpent.toNumber());
   }
 }
