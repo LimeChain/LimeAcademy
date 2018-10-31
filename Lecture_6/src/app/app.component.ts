64b897b2dfab91af975f9109dd7639d489a01777
Ognyan Chikov <o.chikov@propy.com>
2018-10-31 16:33:27 +0200

    added upload form

--- Lecture_6/src/app/app.component.ts
+++ Lecture_6/src/app/app.component.ts
@@ -147,6 +147,26 @@ export class AppComponent {
     downloader.click();
   }
 
+  public upload() {
+    const reader = new FileReader();
+    reader.onloadend = function () {
+      const ipfs = window.IpfsApi('localhost', 5001) // Connect to IPFS
+      const buf = buffer.Buffer(reader.result) // Convert data into buffer
+
+      ipfs.files.add(buf, (err, result) => { // Upload buffer to IPFS
+        if (err) {
+          console.error(err);
+          return;
+        }
+        let url = `http://localhost:8080/ipfs/${result[0].hash}`;
+        console.log(`Url --> ${url}`)
+        document.getElementById("pictureHash").value = result[0].hash;
+      })
+    }
+    const photo = document.getElementById("photo");
+    reader.readAsArrayBuffer(photo.files[0]); // Read Provided File
+  }
+
 }
 
 
