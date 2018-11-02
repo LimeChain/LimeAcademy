3f5209d9fe0bccd54764672876c282ce1092a302
danielivanov <daniel.k.ivanov95@gmail.com>
2018-11-02 13:08:41 +0200

    revert billboard in lecture 4

--- Lecture_4/contracts/Billboard.sol
+++ Lecture_4/contracts/Billboard.sol
@@ -5,18 +5,17 @@ import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
 contract Billboard is Ownable {
 
-    uint256 public price = 50 wei;
+    uint256 public price = 1 ether;
     address public billboardOwner;
     address[] public historyOfOwners;
     mapping(address => uint256) public moneySpent;
     string public slogan;
-    string public ipfsHash;
 
     /**
      * events
      */
 
-    event LogBillboardBought(address buyer, uint256 paied, string slogan, string ipfsHash);
+    event LogBillboardBought(address buyer, uint256 paied, string slogan);
     event LogWithdrawal(uint256 amount, uint256 timestamp);
 
     /**
@@ -32,16 +31,15 @@ contract Billboard is Ownable {
      * functions
      */
 
-    function buy(string newSlogan, string _ipfsHash) public payable {
+    function buy(string newSlogan) public payable {
         require(msg.value >= price, "The ether sent was too low");
 
         billboardOwner = msg.sender;
         historyOfOwners.push(msg.sender);
         moneySpent[msg.sender] += msg.value;
         slogan = newSlogan;
-        ipfsHash = _ipfsHash;
 
-        emit LogBillboardBought(msg.sender, msg.value, newSlogan, ipfsHash);
+        emit LogBillboardBought(msg.sender, msg.value, newSlogan);
     }
 
     function setPrice(uint256 newPrice) public onlyOwner onlyPositive(newPrice) {
